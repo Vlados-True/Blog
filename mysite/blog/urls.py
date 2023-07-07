7204fb5eb64857d513e2d6744639d918111c563d
Vlados-True <vlad.gubine@gmail.com>
2023-07-07 17:47:23 +0300

    finish

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,14 +3,13 @@ from . import views
 from .feeds import LatestPostsFeed
 
 app_name = 'blog'
+
 urlpatterns = [
-    # представления поста
+    # Post views
     path('', views.post_list, name='post_list'),
     # path('', views.PostListView.as_view(), name='post_list'),
-
     path('tag/<slug:tag_slug>/',
          views.post_list, name='post_list_by_tag'),
-
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
          name='post_detail'),
@@ -20,5 +19,4 @@ urlpatterns = [
          views.post_comment, name='post_comment'),
     path('feed/', LatestPostsFeed(), name='post_feed'),
     path('search/', views.post_search, name='post_search'),
-
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,24 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, \
     PageNotAnInteger
 from django.views.generic import ListView
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-
-
-class PostListView(ListView):
-    """
-    Альтернативное представление списка постов
-    """
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
 
 
 def post_list(request, tag_slug=None):
@@ -27,19 +18,16 @@ def post_list(request, tag_slug=None):
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
         post_list = post_list.filter(tags__in=[tag])
-
-    # Постраничная разбивка с 3 постами на страницу
+    # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
-        # Если page_number не целое число, то
-        # выдать первую страницу
+        # If page_number is not an integer deliver the first page
         posts = paginator.page(1)
     except EmptyPage:
-        # Если page_number находится вне диапазона, то
-        # выдать последнюю страницу
+        # If page_number is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
@@ -54,12 +42,13 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    # Список активных комментариев к этому посту
+
+    # List of active comments for this post
     comments = post.comments.filter(active=True)
-    # Форма для комментирования пользователями
+    # Form for users to comment
     form = CommentForm()
 
-    # Список схожих постов
+    # List of similar posts
     post_tags_ids = post.tags.values_list('id', flat=True)
     similar_posts = Post.published.filter(tags__in=post_tags_ids) \
         .exclude(id=post.id)
@@ -73,29 +62,37 @@ def post_detail(request, year, month, day, post):
                    'similar_posts': similar_posts})
 
 
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
 def post_share(request, post_id):
-    # Извлечь пост по идентификатору id
-    post = get_object_or_404(Post,
-                             id=post_id,
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id,
                              status=Post.Status.PUBLISHED)
     sent = False
 
     if request.method == 'POST':
-        # Форма была передана на обработку
+        # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
-            # Поля формы успешно прошли валидацию
+            # Form fields passed validation
             cd = form.cleaned_data
-            post_url = request.build_absolute_uri(
-                post.get_absolute_url())
+            post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = f"{cd['name']} recommends you read " \
                       f"{post.title}"
             message = f"Read {post.title} at {post_url}\n\n" \
                       f"{cd['name']}\'s comments: {cd['comments']}"
-            send_mail(subject, message, 'vlad.gubine@gmail.com',
+            send_mail(subject, message, 'your_account@gmail.com',
                       [cd['to']])
             sent = True
-            # ... отправить электронное письмо
+
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post,
@@ -105,19 +102,17 @@ def post_share(request, post_id):
 
 @require_POST
 def post_comment(request, post_id):
-    '''comments'''
-    post = get_object_or_404(Post,
-                             id=post_id,
+    post = get_object_or_404(Post, id=post_id,
                              status=Post.Status.PUBLISHED)
     comment = None
-    # Комментарий был отправлен
+    # A comment was posted
     form = CommentForm(data=request.POST)
     if form.is_valid():
-        # Создать объект класса Comment, не сохраняя его в базе данных
+        # Create a Comment object without saving it to the database
         comment = form.save(commit=False)
-        # Назначить пост комментарию
+        # Assign the post to the comment
         comment.post = post
-        # Сохранить комментарий в базе данных
+        # Save the comment to the database
         comment.save()
     return render(request, 'blog/post/comment.html',
                   {'post': post,
@@ -129,13 +124,13 @@ def post_search(request):
     form = SearchForm()
     query = None
     results = []
+
     if 'query' in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                similarity=TrigramSimilarity('title', query), ).filter(similarity__gt=0.1).order_by('-similarity')
 
     return render(request,
                   'blog/post/search.html',
